436340f2b80907fa6c731f2375b31c3a8286ee99
jaeminhwang-debug <jaemin.hwang@hotmail.com>
2020-11-14 22:28:43 +0900

    Change calling add_field()
    -

--- django_plotly/graph/views.py
+++ django_plotly/graph/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 import plotly.express as px
-from ctypes import c_uint16, c_uint32
 from . import bin2real
 
 # Create your views here.
@@ -8,12 +7,12 @@ def plot(request):
 
     # Make binary structure
     bs = bin2real.CustomBinStructure()
-    bs.add_field('use22bits', c_uint32, 22)
-    bs.add_field('use10bits', c_uint32, 10)
-    bs.add_field('', c_uint16, 7)
-    bs.add_field('use2bits', c_uint16, 2)
-    bs.add_field('', c_uint16, 4)
-    bs.add_field('use3bits', c_uint16, 3)
+    bs.add_field('use22bits', 22)
+    bs.add_field('use10bits', 10)
+    bs.add_field('', 7)
+    bs.add_field('use2bits', 2)
+    bs.add_field('', 4)
+    bs.add_field('use3bits', 3)
     bs.make_binstructure()
 
     # Read sample file
